be6a5b7b7b1153889d9006facbf5067f0d2e03b4
Yelizaveta-T <61255020+Yelizaveta-T@users.noreply.github.com>
2020-06-10 21:16:25 +0300

    Add files via upload

---
+++ HW#10/main.js
@@ -0,0 +1,7 @@
+let div = document.createElement('div');
+  div.innerHTML='<b>Тихоновская Елизавета Дмитриевна</b>';
+  document.body.prepend(div);
+
+let elem = document.createElement('p');
+  elem.innerHTML='<b>24.04.1999</b>';
+  some_div.append(elem);
